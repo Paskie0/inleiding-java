37b5a7c3140094db83a6b4c297b707617f9f2902
Pascal <2114466@talnet.nl>
2019-09-06 11:35:14 +0200

    hoofdstuk 4 - 1,2 en 3

--- src/h02/AppletsOfzo.java
+++ src/h02/AppletsOfzo.java
@@ -3,15 +3,12 @@ package h02;
 import java.awt.*;
 import java.applet.*;
 
-//een klasse met de naam Show van het type Applet
 public class AppletsOfzo extends Applet {
 
-    //een (lege) methode die de Applet initialiseert
     public void init() {
         setBackground(Color.white);
     }
 
-    //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
         g.setColor(Color.blue);
         g.drawString("Pascal ", 150, 150);
